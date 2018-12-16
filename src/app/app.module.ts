f3c871d736a0252fcd78466dd20c31b60f30ee2f
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-12-16 22:01:55 +0200

    Add Inventory Component and Start to Implement Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MyModalComponent } from './common/my-modal/my-modal.component';
+import { RouterModule, Routes} from '@angular/router';
+import { InventoryComponent } from './inventory/inventory.component';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'inventory', component: InventoryComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MyModalComponent
+    MyModalComponent,
+    InventoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
