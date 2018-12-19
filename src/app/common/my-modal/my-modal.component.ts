c35a97f80bd232d862581bec4ab15bc02740671f
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-12-19 18:20:22 +0200

    Started with my-modal

--- src/app/common/my-modal/my-modal.component.ts
+++ src/app/common/my-modal/my-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-modal',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-modal.component.css']
 })
 export class MyModalComponent implements OnInit {
+
+    private _modalId: string;
+
+    @Input()
+    get modalId(): string {
+        return this._modalId;
+    }
+    set modalId(value: string) {
+        this._modalId = value;
+        // document.querySelector('.modal').setAttribute('id', value);
+    }
 
   constructor() { }
 
