2b094f54a1e6d7aef5f33569893984fdc1da8a72
Florian Prechtl <florian.prechtl@st.oth-regensburg.de>
2018-12-20 11:17:34 +0200

    show inventory entries

--- src/app/inventory/inventory-show-case/inventory-show-case.component.ts
+++ src/app/inventory/inventory-show-case/inventory-show-case.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-inventory-show-case',
-  templateUrl: './inventory-show-case.component.html',
-  styleUrls: ['./inventory-show-case.component.css']
+    selector: 'app-inventory-show-case',
+    templateUrl: './inventory-show-case.component.html',
+    styleUrls: ['./inventory-show-case.component.css']
 })
 export class InventoryShowCaseComponent implements OnInit {
 
-  constructor() { }
+    public inventoryentries;
 
-  ngOnInit() {
-  }
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit() {
+        this.http.get('http://localhost:8080/projects/RESTAPI/inventoryentry').subscribe((res) => {
+            console.log(res);
+            this.inventoryentries = res;
+        });
+    }
 
 }
